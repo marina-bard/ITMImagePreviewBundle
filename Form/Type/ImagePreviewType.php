3f7e31d193851e3abbeaa0df921a71d0556143fd
Alexander Samusevich <aleksander.samusevich@itmhouse.com>
2014-08-20 11:30:55 +0300

    thumb only

--- Form/Type/ImagePreviewType.php
+++ Form/Type/ImagePreviewType.php
@@ -62,7 +62,7 @@ class ImagePreviewType extends AbstractType
 
                             foreach( $liipFilters[$filter['name']]['filters'] as $liipFilterName => $liipFilter )
                             {
-                                if(!in_array($liipFilterName, ['crop', 'thumbnail'])) throw new Exception('ITMImagePreview support crop and thumbnail liip filters only!');
+                                if(!in_array($liipFilterName, ['thumbnail'])) throw new Exception('ITMImagePreview support thumbnail liip filters only!');
                                 $filter = array_merge($filter, $liipFilter);
                             }
 
